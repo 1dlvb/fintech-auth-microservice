f0a78d47dbc8da125246a35889f8950b7ada1fe4
1dlvb <matushkin-2017@mail.ru>
2024-07-25 18:02:30 +0500

    chore: initial commit

--- src/main/java/com/fintech/auth/AuthApplication.java
+++ src/main/java/com/fintech/auth/AuthApplication.java
@@ -3,6 +3,7 @@ package com.fintech.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
 public class AuthApplication {
 
